4ddc35f8c777f8f8947777b0ff7bc6127a3b9026
Adam Slack <adamslack@outlook.com>
2017-11-28 15:47:47 +0000

    initial DB structure

--- database/init.sql
+++ database/init.sql
@@ -1,81 +1,124 @@
 begin;
+----------------------------------------------------
+--  Clinician And Participant Information
+--  
+----------------------------------------------------
 
+
+-----
+--  Example: participant_id: 12345
+-----
 create table participants(
-    p_id    int64   not null primary key,
-    c_id    int64   not null references clinicians(c_id)
+    participant_id  int64   not null primary key,
 );
 
+-----
+--  Example: clinician_id: 54321
+-----
 create table clinicians(
-    c_id    int64   not null primary key
+    clinician_id    int64   not null primary key,
 );
 
+-----
+--  Example: test_id: 1111, participant_id: 12345, clinician_id: 54321
+-----
 create table participant_tests(
-    p_id    int64   not null references participants(p_id),
-    t_id    serial  not null,
-    c_id    int64   not null, references clinican(c_id),
-    primary Key (p_id, c_id, t_id)
-)
+    test_id         serial  not null primary key,
+    participant_id  int64   not null references participants(participant_id),
+    clinician_id    int64   not null, references clinican(clinician_id),
+);
 
+----------------------------------------------------
+--  Test Result Information
+--
+----------------------------------------------------
+
+-----
+--  Example: test_id: 1111, time_taken: 900, true_pos: 12, false_pos: 13, false_neg: 15
+-----
 create table dot_cancellation(
-    t_id        int64   not null references participant_tests(p_id),
-    time_taken  int64   not null ,
+    test_id     serial  not null references participant_tests(test_id) primary key,
+    time_taken  int64   not null,
     true_pos    int64   not null,
     false_pos   int64   not null,
     false_neg   int64   not null,
-    check(false_pos > -1),
-    check(false_neg > -1),
-    check(true_pos > -1),
-    check(time_taken < 901)
 );
 
 create table car_directions(
-    t_id        int64   not null references participant_tests(p_id),
+    test_id     serial  not null references participant_tests(test_id)  primary key,
     time_taken  int64   not null,
     points      int64   not null,
-    check(points > -1),
-    check(points < 33)
 );
 
 create table compass_directions(
-    t_id        int64   not null references participant_tests(p_id),
+    test_id     serial  not null references participant_tests(test_id)  primary key,
     time_taken  int64   not null,
     points      int64   not null,
-    check(points > -1),
-    check(points < 33)
 );
 
 create table road_scenarios(
-    t_id        int64   not null references participant_tests(p_id),
+    test_id     serial  not null references participant_tests(test_id) primary key,
     time_take   int64   not null,
     points      int64,
-    check(points < 13)
 );
 
 create table trail_making(
-    t_id        int64   not null references participant_tests(p_id),
+    test_id     serial  not null references participant_tests(test_id)  primary key,
     time_taken  int64   not null,
     mistakes    int64   not null,
 );
 
-create table test_results(
-    t_id        int64 not null references participant_tests(t_id),
-    p_id        int64 not null references participants(p_id),
-    c_id        int64 not null references clinicians(c_id),
+----------------------------------------------------
+-- Localisation Presets
+--
+----------------------------------------------------
+
+-----
+--  Example: preset_id: 0001, region: en-gb, localisation: { dot_cancellation: {instructions: 'fooo baar'}}
+-----
+create table localisation_presets(
+    preset_id       serial  not null primary key,
+    region          text    not null,
+    localisation    jsonb   not null,
 );
 
+----------------------------------------------------
+--  Test Interaction Recordings
+--
+----------------------------------------------------
 
-
-create table algorithms_names(
-    a_id        int64 not null primary key,
-    string      text  not null unique,
-);
-
+-----
+--  Example: test_id: 1111, interaction: {dot_cancellation: [{type: 'mouse_down', x: 123, y: 321}, {type: 'mouse_move', x: 124, y: 322}]}
+-----
 create table test_interactions(
-    t_id        int64   not null references participant_tests(p_id),
-    interaciton jsonb   not null
+    test_id      serial  not null references participant_tests(test_id) primary key,
+    interaction  jsonb   not null,
 );
 
-create table 
+----------------------------------------------------
+--  Results for the 
+--
+----------------------------------------------------
+
+-----
+--  Example: test_id: 1111, algorithm_id: 2222, results: {recommendation_score: -0.004, probability: 0.005, recommend: 'No'}
+-----
+create table algorithm_results(
+    test_id         serial,
+    algorthim_id    int64,
+    results         jsonb,
+    primary key(test_id)
+);
+
+-----
+--  Example: algorithm_id: 2222, clinician_id: 54321, algorithm_name: 'SDSA'
+-----
+create table algorithms(
+    algorithm_id    serial not null primary key,
+    clinician_id    int64  not null references clinicians(clinician_id),
+    algorithm_name  text   not null
+)
+
 
 
 commit;
