15ddec7a0cf4297d7b1b73bd7a3dafaac2b59553
Adam Slack <adamslack@outlook.com>
2017-11-28 16:43:33 +0000

    Participant application initial changes (#6)
    * c sharp hello world
    
    * initial init sql
    
    * db changes
    
    * db
    
    * initial DB structure
    
    * removed default stuff
    
    * participant
    
    * oopsie
    
    * initial components set up
    
    * initial components
    
    * bleugh

--- participant/src/app/app.module.ts
+++ participant/src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TestInstructionsComponent } from './test-instructions/test-instructions.component';
+import { DotCancellationTestComponent } from './dot-cancellation-test/dot-cancellation-test.component';
+import { CarDirectionsTestComponent } from './car-directions-test/car-directions-test.component';
+import { CompassDirectionsTestComponent } from './compass-directions-test/compass-directions-test.component';
+import { RoadScenariosTestComponent } from './road-scenarios-test/road-scenarios-test.component';
+import { TrailMakingTestComponent } from './trail-making-test/trail-making-test.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestInstructionsComponent,
+    DotCancellationTestComponent,
+    CarDirectionsTestComponent,
+    CompassDirectionsTestComponent,
+    RoadScenariosTestComponent,
+    TrailMakingTestComponent,
+    LoginScreenComponent
   ],
   imports: [
     BrowserModule
---
+++ participant/src/app/dot-cancellation-test/dot-cancellation-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dot-cancellation-test',
+  templateUrl: './dot-cancellation-test.component.html',
+  styleUrls: ['./dot-cancellation-test.component.scss']
+})
+export class DotCancellationTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
