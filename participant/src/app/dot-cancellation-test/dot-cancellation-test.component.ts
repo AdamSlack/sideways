b6d983a6b6581365c8b7770cdf3d36849a2c6166
Adam Slack <adamslack@outlook.com>
2017-11-28 16:39:53 +0000

    bleugh

--- participant/src/app/dot-cancellation-test/dot-cancellation-test.component.ts
+++ participant/src/app/dot-cancellation-test/dot-cancellation-test.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dot-cancellation-test',
   templateUrl: './dot-cancellation-test.component.html',
-  styleUrls: ['./dot-cancellation-test.component.css']
+  styleUrls: ['./dot-cancellation-test.component.scss']
 })
 export class DotCancellationTestComponent implements OnInit {
 
