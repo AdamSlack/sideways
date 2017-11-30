5bff347238d6f1b8972962170016d9e35f2800ef
Adam Slack <adamslack@outlook.com>
2017-11-30 17:23:16 +0000

    login component changes

--- participant/src/app/login-screen/login-screen.component.ts
+++ participant/src/app/login-screen/login-screen.component.ts
@@ -8,12 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class LoginScreenComponent implements OnInit {
 
 
+  // PLACE HOLDERS
   public p_id : string;
+  public password : string;
+  public c_id : string;
 
   constructor() { }
 
   public connectToSDSA(){
     console.log('Participant ID: ' + this.p_id);
+    console.log('Password:' + this.password)
+    console.log('Clinician ID: ' + this.c_id);
   }
 
   ngOnInit() {
