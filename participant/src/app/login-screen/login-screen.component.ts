8a276aba9c4e60624a2a58b45232435a52886f70
Adam Slack <adamslack@outlook.com>
2017-11-30 15:59:03 +0000

    login component additions

--- participant/src/app/login-screen/login-screen.component.ts
+++ participant/src/app/login-screen/login-screen.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
-  styleUrls: ['./login-screen.component.css']
+  styleUrls: ['./login-screen.component.scss']
 })
 export class LoginScreenComponent implements OnInit {
 
+
+  public p_id : string;
+
   constructor() { }
 
+  public connectToSDSA(){
+    console.log('Participant ID: ' + this.p_id);
+  }
+
   ngOnInit() {
+
   }
 
 }
