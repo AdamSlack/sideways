bd37fd33579c3091b0823f6f22dff2fc73b6b826
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2017-12-19 14:34:48 +0000

    added the timer to all the tests

--- participant/src/app/compass-directions-test/compass-directions-test.component.ts
+++ participant/src/app/compass-directions-test/compass-directions-test.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ResultsService } from '../services/results.service';
+import { RecordTimingService } from '../services/record-timing.service';
 
 @Component({
   selector: 'app-compass-directions-test',
@@ -7,6 +8,8 @@ import { ResultsService } from '../services/results.service';
   styleUrls: ['./compass-directions-test.component.scss']
 })
 export class CompassDirectionsTestComponent implements OnInit {
+
+  private timer : RecordTimingService;  
 
   constructor(private rs: ResultsService) { }
   
