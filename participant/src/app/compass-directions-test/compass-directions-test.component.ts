911f3605dce34ad20959afd7362bae2183df4056
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2017-12-22 15:16:11 +0000

    Example compass directions matrix based interaction game using fabric js (#40)
    Implementation of Matrix Based Game Proof of Concept.

--- participant/src/app/compass-directions-test/compass-directions-test.component.ts
+++ participant/src/app/compass-directions-test/compass-directions-test.component.ts
@@ -1,5 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ResultsService } from '../services/results.service';
+import { FabricService } from '../services/fabric.service'
+
+import "fabric"
+declare const fabric: any;
+//TODO: place in inherited class the global canvas components. Only ever one canvas on screen
+//TODO: debug mode enables the skeleton view of all objects
+
+//Canvas for displaying things
+var Canvas: any;
+//Should be a json defined classed
+var Deck:any[];
+
+var GridSquares:any[];
+
+//Reads row, column
+enum compassDir {
+  //Row one
+  SouthEastWest= 0,
+  NorthEastWest,
+  SouthWestWest,
+  EastWest,
+  //Row two
+  SouthEastNorthWest,
+  NorthEastNorthWest,
+  SouthWestNorthWest,
+  EastNorthWest,
+  //Row three
+  SouthEastNorth,
+  NorthEastNorth,
+  SouthWestNorth,
+  EastNorth,
+  //Row Four
+  SouthEastSouth,
+  NorthEastSouth,
+  SouthWestSouth,
+  EastWestSouth,
+} 
 
 @Component({
   selector: 'app-compass-directions-test',
@@ -8,13 +45,205 @@ import { ResultsService } from '../services/results.service';
 })
 export class CompassDirectionsTestComponent implements OnInit {
 
-  constructor(private rs: ResultsService) { }
+  constructor(private rs: ResultsService, private fab: FabricService) { }
   
+  ngOnInit() {
+    console.log("requesting a fabric canvas");
+    //this.fab.generateFabricCanvas();
+    //this.canvas = new fabric.Canvas('canvas', { selection: false });
+    Canvas = this.fab.generateFabricCanvas('canvas');
+    Deck = [];
+
+    var grid_length = 300;
+    GridSquares = this.fab.createGridBaseSquares(100,100, Canvas, grid_length,4);
+    this.addCompassImages(Canvas, 100 - (grid_length/4),  100 - (grid_length/4), grid_length/4);
+
+    this.createDeck(Canvas.width - 100, Canvas.width - 110);
+    
+
+    // Commented out cause we don't really need it?
+    //Canvas.add(this.createShuffleButton(Canvas.width - 100, Canvas.width - 150));
+    Canvas.add(this.createDonezoButton(Canvas.width - 100, Canvas.width - 300));
+  }
+
+  
+  private createDeck(xOffset : number = 0, yOffset : number  = 0, deckSize : number = 16) {
+      //Initialise deck of compass cards
+      var cards = Array.from({length: deckSize}, (value, key) => key).map((idx : number) => {
+        let card = this.fab.createReactingObj(Canvas,xOffset,yOffset, 40, 'card' + idx.toString());
+        card.isPlaced = false;
+        card.lockRotation = true;
+        card.lockUniScaling = true;
+        card.selectable = true;
+        card.lockScalingX = true;
+        card.lockScalingY = true
+        Canvas.add(card);
+        Deck.push(card);
+      });
+  }
+
+  private gatherResults() {
+    var squareMatches = [...Array(GridSquares.length||0)].map((v,i)=>i)
+  
+    console.log(squareMatches.length);
+    GridSquares.forEach( square => {
+      Deck.forEach( card => {
+        //card.intersectsWithObject()
+        if (card.intersectsWithObject(square)) {
+          squareMatches[square.id] = card.id;
+        }
+      });
+    });
+    
+    console.log(squareMatches);
+  
+  }
+
+  private createDonezoButton(x: number, y: number) {
+
+    var butt = new fabric.Text( 'Donezo', {
+      left:x,
+      top:y,
+      width:40,
+      height:40,
+      fontSize: 30,
+      font: "roboto",
+      lockMovementX: true,
+      lockMovementY: true,
+      lockRotation: true,
+      lockUniScaling: true,
+      selectable: true,
+      lockScalingX: true,
+      lockScalingY: true
+    });
+    
+    butt.on('selected', options => {
+      this.gatherResults();
+    });
+
+    return butt;
+  }
+  
+  //TODO: in inherited behaviour...
+  private createShuffleButton(xPos: number, yPos: number) {
+    //Create shuffle button
+    var butt = new fabric.Text( 'Shuffle', {
+      left:xPos,
+      top:yPos,
+      width:40,
+      height:40,
+      fontSize: 30,
+      font: "roboto",
+      lockMovementX: true,
+      lockMovementY: true,
+      lockRotation: true,
+      lockUniScaling: true,
+      selectable: true,
+      lockScalingX: true,
+      lockScalingY: true
+    });
+
+    console.log("current deck",Deck);
+
+    //Shuffle and place logic
+    butt.on('selected', function(options) {
+      console.log("shuffling");
+
+      //Shuffle all those in deck that are not in placed state.
+      //XXX: not real random, first deck compoennts are heavily weighted
+      console.log("Current deck", Deck)
+      Deck.sort(function (a,b) {
+        console.log("Deck item:",a);
+        if(a.isPlaced) { 
+          return -1; //pop too front so were only shuffling the non placed section
+        
+        }  else { //give a rnadom sort position
+          //Reset card position
+      
+        //TODO: get origin position from Deck
+        var reset = (obj) => {
+          obj.left = 30;
+          obj.top = 30;
+          //a.animate('top', 45, { duration: 4, onChange:Canvas.renderAll.bind(Canvas) });    
+        }
+
+        reset(a);
+        reset(b);
+
+        console.log("current deck",Deck);          
+        return 0.5 - Math.random();
+        }
+      });
+    });
+
+    return butt;
+  }
+
+  public addCompassImages(canvas: any, xPos: number, yPos: number, compass_length: number) {
+    // fabric.Image.loadSVGFromURL('../assets/compass_north.svg', function(oImg) {
+    //   oImg.width = this.box_length
+      
+    //   oImg.height = this.box_length;
+    //   canvas.add(oImg);
+    // });
+    var compass_url = '../assets/compass_north.svg';
+
+    var group = [];
+    var rotate = 0;
+    var increment_rotation = 45;
+
+    //Top row
+
+    //Just messing... es6
+    // let times=(n,f)=>{while(n-->0)f();}
+    // times (3, console.log('times repeat'));
+    const times = n => f => {
+      let iter = i => {
+        if (i === n) return
+        f (i)
+        iter (i + 1)
+      }
+      return iter (0)
+    }
+    
+    times (4) (i => {
+      fabric.loadSVGFromURL(compass_url, (objects, options) => {
+        var obj = fabric.util.groupSVGElements(objects, {
+          left: xPos + (compass_length/2) + (compass_length * (i+1)),
+          top: yPos + (compass_length/2),
+          originX: 'center', 
+          originY: 'center',
+          selectable: false
+        });
+        console.log(obj.rotate);
+        obj.rotate(rotate);
+        rotate += increment_rotation;
+        obj.scaleToWidth(compass_length);
+        canvas.add(obj).renderAll();
+      });
+    })
+
+    rotate = 0;
+    times (4) (i => {
+      fabric.loadSVGFromURL(compass_url, (objects, options) => {
+        var obj = fabric.util.groupSVGElements(objects, {
+          left: xPos + (compass_length/2),
+          top: yPos + ((compass_length/2)  + (compass_length * (i+1))),
+          originX: 'center', 
+          originY: 'center',
+          selectable: false
+        });
+        obj.rotate(rotate);
+        rotate += increment_rotation;
+        obj.scaleToWidth(compass_length);
+        canvas.add(obj).renderAll();
+      });
+    })
+
+  }
+
+
   public sendResults() {
     this.rs.insertCompassDirectionResults(1, 123, 456);
   }
-  
-  ngOnInit() {
-  }
-
 }
---
+++ participant/src/app/services/fabric.service.ts
@@ -0,0 +1,132 @@
+import { Injectable } from '@angular/core';
+import { deprecate } from 'util';
+import "fabric"
+import { startTimeRange } from '@angular/core/src/profile/wtf_impl';
+declare const fabric: any;
+
+/*  
+  Note: would be singleton service
+  TODO: any to a canvas type..
+*/
+@Injectable()
+export class FabricService {
+
+  constructor() { }
+
+  public generateFabricCanvas(id: string) {
+    let canvas = new fabric.Canvas(id);
+    return canvas;
+  }
+
+  public createReactingObj(canvas: any, x: number, y: number, length: number, identifer: string) {
+      // create a rectangle object
+      var card = new fabric.Rect({
+        left: x, 
+        top: y, 
+        width: length, 
+        height: length, 
+        fill: '#ffb366', 
+        originX: 'left', 
+        originY: 'top',
+        centeredRotation: true,
+        lockUniScaling: true,
+        lockScalingY: true, 
+        lockScalingX: true,       
+        id: identifer,
+      });
+
+      card.type = "card"
+      /* Card interaction logic */
+      card.on('mousedown', function(options) {
+        card.setShadow({ color:"rgba(0,0,0,0.3)",blur:20,offsetX:2,offsetY:2 });            
+        card.animate('angle', '4', { onChange: canvas.renderAll.bind(canvas) });    
+      });
+
+
+      card.on('mouseup', function(options) {
+        card.setShadow(null);
+        card.animate('angle', '0', { onChange: canvas.renderAll.bind(canvas) });  
+      });
+
+
+      //Intesection colissions ....
+      canvas.on({
+        'object:moving': onChange,
+        'object:scaling': onChange,
+        'object:rotating': onChange,
+        //Touch:gesture
+      });
+
+      function onChange(options) {
+        options.target.setCoords();
+        canvas.forEachObject(function(obj) {
+          if (obj === options.target) return;
+          obj.set('opacity' ,options.target.intersectsWithObject(obj) ? 0.5 : 1);
+        });
+      }
+
+      return card;
+  }
+
+  // public createGridBaseLines(canvas: any, gridSize: number) {
+  //   this.box_length = canvas.width / gridSize;
+  //   for (var i = 0; i < (canvas.width / this.box_length); ++i) {
+  //     canvas.add(new fabric.Line([ i * this.box_length, 0, i * this.box_length, canvas.width], { stroke: '#ccc', selectable: false }));
+  //     canvas.add(new fabric.Line([ 0, i * this.box_length, canvas.height, i * this.box_length], { stroke: '#ccc', selectable: false }))
+  //   }
+  // }
+
+
+  public createGridBaseSquares(xPos: number, yPos: number, canvas: any, side_length: number, squares: number) {
+
+    var square_length = side_length / squares; 
+    console.log(square_length);
+    var start_x = 0;
+    var start_y = 0;
+
+    var squareSet = [];
+    var id = 0; //All grid parts are id 0, ..., Squares. This to map nicely to a enum and therefore see specfic test enum for id tranlation.
+    while(start_y < side_length) {
+      while(start_x < side_length) {
+        var rect = new fabric.Rect({
+          left: xPos + start_x, 
+          top: yPos + start_y, 
+          width: square_length,
+          height: square_length,
+          selectable: false,
+          id: id++,
+          fill: '#99ccff'
+        });
+        squareSet.push(rect);
+        start_x += square_length;
+        console.log("created square",rect.id);
+        canvas.add(rect); 
+      }
+      start_x = 0;
+      start_y += square_length;
+    }
+
+    return squareSet;
+  }
+
+  
+  /*
+  @deprecate why even even activate snapping though?
+  */
+  // public activateSnapping(canvas: any) {
+  //   if (typeof this.box_length === "undefined") {
+  //     console.log('Have not initilaised box_length');      
+  //   } else {
+  //     console.log('locking enabled');
+  //     canvas.on('object:moving', function(options) { 
+  //       options.target.set({
+  //         left: Math.round(options.target.left / this.box_length) * this.box_length,
+  //         top: Math.round(options.target.top / this.box_length) * this.box_length
+  //       });
+  //     });
+  //   }
+  // }
+
+}
+
+
