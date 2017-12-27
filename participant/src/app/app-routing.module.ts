467aa60793de846037caad6686e1f5041bb2770b
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2017-12-27 16:41:19 +0000

    Just customised for the test routes - need to consider login page location etc

--- participant/src/app/app-routing.module.ts
+++ participant/src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+// Tests
+import { DotCancellationTestComponent } from './dot-cancellation-test/dot-cancellation-test.component';
+import { CarDirectionsTestComponent } from './car-directions-test/car-directions-test.component';
+import { CompassDirectionsTestComponent } from './compass-directions-test/compass-directions-test.component';
+import { RoadScenariosTestComponent } from './road-scenarios-test/road-scenarios-test.component';
+import { TrailMakingTestComponent } from './trail-making-test/trail-making-test.component';
+
+const TestRoutes = [{
+  path:'dot_cancellation', component: DotCancellationTestComponent
+}, {
+  path:'car_directions', component: CarDirectionsTestComponent
+}, {
+  path:'compass_directions', component: CompassDirectionsTestComponent
+}, {
+  path:'road_scenarios', component: RoadScenariosTestComponent
+}, {
+  path:'trail_making', component: TrailMakingTestComponent
+}]
+
+//Create the desired routeing
+const routes: Routes = TestRoutes;
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],  
+  exports: [ RouterModule ],
 })
 export class AppRoutingModule { }
+
+
