db7963afa8efa243061b610482d9e9f4397a31d6
Ben Hendley <benjaminhendley@gmail.com>
2018-01-08 22:25:55 +0000

    Initial localisation file endpoints

---
+++ server/server/SDSA/Repository/Interfaces/ILocalisationRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using SDSA.Models;
+
+namespace SDSA.Repository.Interfaces 
+{
+    public interface ILocalisationRepository
+    {
+         LocalisationImage GetImage(int imageId);
+         LocalisationImage GetImage(int localisationId, string ImageName);
+         IEnumerable<ImageDescription> GetImageIdAndNameByLocalisationid(int localisationId);
+        void SaveImage(LocalisationImage LI);
+    }
+}
---
+++ server/server/SDSA/Service/Localisationservice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SDSA.Models;
+using SDSA.Repository.Interfaces;
+using SDSA.Service.Interfaces;
+namespace SDSA.Service
+{
+    public class Localisationservice : ILocalisationService
+    {
+        private readonly ILocalisationRepository _localisationRepository;
+        public Localisationservice(ILocalisationRepository localRepo)
+        {
+            _localisationRepository = localRepo;
+        }
+        public LocalisationImage GetImage(int localisationid)
+             => _localisationRepository.GetImage(localisationid);
+
+        public LocalisationImage GetImage(int localisationId, string ImageName)
+            => _localisationRepository.GetImage(localisationId, ImageName);
+
+        public IEnumerable<ImageDescription> GetImageIdAndNameByLocalisationid(int localisationId)
+            => _localisationRepository.GetImageIdAndNameByLocalisationid(localisationId);
+        
+        public void SaveImage(LocalisationImage LI)
+             => _localisationRepository.SaveImage(LI);
+    }
+}
