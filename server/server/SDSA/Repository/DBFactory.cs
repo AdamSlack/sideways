72508eebf3e547b14a409ef9a4414bbe840fe691
Adam Slack <adamslack@outlook.com>
2018-01-18 09:49:41 +0000

    added switch

--- server/server/SDSA/Repository/DBFactory.cs
+++ server/server/SDSA/Repository/DBFactory.cs
@@ -27,6 +27,8 @@ namespace SDSA.Repository
                  return new SqlConnection(connectionString);
                 case "PostGreSQL1":
                     return new NpgsqlConnection(connectionString);
+                case "test_local_db":
+                    return new NpgsqlConnection(connectionString);
                 default:
                     throw new Exception("No handler for that connection string.");
             }
