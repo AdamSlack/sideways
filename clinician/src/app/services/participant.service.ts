3eae23af16e960234eb9431a8feb25854d1c0d3c
Adam Slack <adamslack@outlook.com>
2018-02-02 20:42:30 +0000

    weird...

--- clinician/src/app/services/participant.service.ts
+++ clinician/src/app/services/participant.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { AuthenticationService } from './authentication.service'; 
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class ParticipantService {
+export class Participantervice {
 
-  constructor() { }
+  constructor(
+    public auth : AuthenticationService,
+    public http : HttpClient  
+  ) { }
+
+  public requestClinicianParticipant() : Observable<Array<string>> {
+    let url = this.auth.ROOT + '/Participant/clinician' + this.auth.CLINICIAN_ID;
+    let headers = this.auth.createHeaders();
+    return this.http.get<any>(url);
+  }
+
+  public requestParticipantTests(p_id : number) : Observable<Array<string>> {
+    let url = this.auth.ROOT + '/Participant/' + p_id + '/Tests';
+    return this.http.get<any>(url);
+  }
+3
+  public requestParticipantTestTypes(t_id : number) : Observable<Array<string>> {
+    let url = this.auth.ROOT + '/Participant/tests/types/' + t_id;
+    return this.http.get<any>(url);
+  }
+
+  public requestTestResults(t_id : number, testType : number) {
+    let url = this.auth.ROOT + '/Participant/results/' + t_id + '/' +  testType;
+    return this.http.get<any>(url);
+  }
 
 }
--- server/server/SDSA/Repository/Interfaces/IParticipantRepository.cs
+++ server/server/SDSA/Repository/Interfaces/IParticipantRepository.cs
@@ -8,5 +8,7 @@ namespace SDSA.Repository.Interfaces
         IEnumerable<int> GetParticipantTests(int participantId);
         int CreateParticipantTest(ParticipantTest PT);
         int CreateParticipant();
+
+        IEnumerable<int> GetClinicianParticipants(int ClinicianID);
     }
 }
