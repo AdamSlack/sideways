15542f31be3dba6d45e440843f388fd364ef5ea7
James Allen <james2826@hotmail.com>
2018-02-14 12:18:04 +0000

    removed unecessary ParticipantTestID casting

--- participant/src/app/components/trail-making-test/trail-making-test.component.ts
+++ participant/src/app/components/trail-making-test/trail-making-test.component.ts
@@ -519,12 +519,8 @@ export class TrailMakingTestComponent implements OnInit
 
   sendResults()
   {
-    //Endpoint Expects:
-    //insertTrailMaking(p_id: number, time_taken: number, mistakes: number)
     var timeTaken = this.test1timer.getTimeElapsed(true) + this.test2timer.getTimeElapsed(true);
     var totalMistakes = this.test1Mistakes + this.test2Mistakes;
-    var participantID : number = parseInt(this.auth.PARTICIPANT_TEST_ID);
-    console.log("Participant ID: ", participantID);
     this.rs.insertTrailMaking(this.auth.PARTICIPANT_TEST_ID,timeTaken,totalMistakes);
     console.log("Results sent");
   }
