bb2be302b21a566050acecf618fcad1865ca0e17
Jayan Mistry <mistryjayan@gmail.com>
2018-02-14 12:11:08 +0000

    :

--- participant/src/app/components/dot-cancellation-test/dot-cancellation-test.component.ts
+++ participant/src/app/components/dot-cancellation-test/dot-cancellation-test.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ResultsService } from '../../services/results.service';
 import { RecordTimingService } from '../../services/record-timing.service';
 
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dot-cancellation-test',
@@ -12,16 +11,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class DotCancellationTestComponent implements OnInit {
 
   private timer : RecordTimingService;
-  
-  constructor(private rs: ResultsService, private _router: Router, 		private route: ActivatedRoute  ) { }
 
-  public sendResults() {
-    //this.rs.insertDotCancellationResults("1", 123, 1, 2, 3);
-    console.log(this._router.url)
-    console.log(this.route)
-    // this._router.navigate([ '../'],  { relativeTo: this.route });
-    this._router.navigateByUrl('/car_directions');
-  }
+  constructor(private rs: ResultsService) { }
+
   
   ngOnInit() {
   }
