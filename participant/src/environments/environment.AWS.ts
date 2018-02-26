67301cd9941c50caa590fd13c2b649cbec5802fd
Ubuntu <ubuntu@ip-172-31-32-107.eu-west-2.compute.internal>
2018-02-26 12:49:32 +0000

    minor changes

--- participant/src/environments/environment.AWS.ts
+++ participant/src/environments/environment.AWS.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  APIBaseUri: "http://ec2-35-176-147-214.eu-west-2.compute.amazonaws.com/API/"
-
+  APIBaseUri: "http://ec2-35-176-147-214.eu-west-2.compute.amazonaws.com/API/",
+  IMGBaseUri: "http://ec2-35-176-147-214.eu-west-2.compute.amazonaws.com/"
 };
