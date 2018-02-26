a34443d52f9945b820cf13b1f7b45a8098177de4
Ubuntu <ubuntu@ip-172-31-32-107.eu-west-2.compute.internal>
2018-02-26 12:47:15 +0000

    minor changes

--- clinician/src/environments/environment.AWS.ts
+++ clinician/src/environments/environment.AWS.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  APIBaseUri: "ec2-35-176-147-214.eu-west-2.compute.amazonaws.com/"
+  APIBaseUri: "http://ec2-35-176-147-214.eu-west-2.compute.amazonaws.com/API/"
 };
